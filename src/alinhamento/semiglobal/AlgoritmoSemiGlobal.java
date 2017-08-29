44a7db476d145e7078c28abf92a020c510554e15
Mateus Fittipaldi <mateusfitti@gmail.com>
2017-08-29 16:24:33 -0300

    Done
    99% pronto (o 1% é melhorias, o que não vamos fazer)!

--- src/alinhamento/semiglobal/AlgoritmoSemiGlobal.java
+++ src/alinhamento/semiglobal/AlgoritmoSemiGlobal.java
@@ -1,7 +1,7 @@
 package alinhamento.semiglobal;
 
 public class AlgoritmoSemiGlobal {
-	
+
 	// declaracao das variaveis
 	private int gap, match, mismatch; // custos
 	private int m, n; // tamanho das sequencias
@@ -43,23 +43,24 @@ public class AlgoritmoSemiGlobal {
 						Math.max(matriz[i - 1][j] + gap, matriz[i][j - 1] + gap));
 			}
 		}
-		
+
 		// busca pela pontuacao maxima de alinhamento otimo na ultima linha e coluna
 		maximo = Integer.MIN_VALUE;
-		
+
 		for (i = 0; i <= n; i++) {
 			if (matriz[i][m] > maximo) {
 				maximo = matriz[i][m];
 			}
 		}
-		for (j = 0; j <= m-1; j++) {
+		for (j = 0; j <= m - 1; j++) {
 			if (matriz[n][j] > maximo) {
 				maximo = matriz[n][j];
 			}
 		}
 	}
 
-	// metodo que printa a matriz e a pontuacao maxima de alinhamento otimo no console
+	// metodo que printa a matriz e a pontuacao maxima de alinhamento otimo no
+	// console
 	public void imprimeMatriz() {
 		for (int i = 0; i <= n; i++) {
 			for (int j = 0; j <= m; j++) {
@@ -73,33 +74,47 @@ public class AlgoritmoSemiGlobal {
 		}
 		System.out.println("\n" + "ALINHAMENTO OTIMO = " + maximo);
 	}
-	
-	// metodo que percorre a matriz da ultima posicao ate a primeira posicao atraves do alinhamento otimo
-		public void otimo() {
-			int i = n;
-			int j = m;
-			int c = 0;
-			seq = new int[n + m + 2];
-			while (i != 0 || j != 0) {
-				if (i != 0 && j != 0) {
-					if (matriz[i][j] == matriz[i - 1][j - 1] + match || matriz[i][j] == matriz[i - 1][j - 1] + mismatch) {
-						seq[c] = 1;
-						c++;
-						i--;
-						j--;
-					} else {
-						if (matriz[i][j] == matriz[i - 1][j] + gap) {
-							seq[c] = 2;
-							c++;
-							i--;
-						} else {
-							seq[c] = 3;
-							c++;
-							j--;
-						}
-					}
+
+	// metodo que percorre a matriz da ultima posicao ate a primeira posicao atraves
+	// do alinhamento otimo
+	public void otimo() {
+		int i = n;
+		int j = m;
+		int c = 0;
+		seq = new int[n + m + 2];
+		while (matriz[i][j] != maximo) {
+			if (i == 0) {
+				i = n;
+				c = 0;
+				break;
+			} else {
+				seq[c] = 2;
+				c++;
+				i--;
+			}
+		}
+		if (matriz[i][j] != maximo) {
+			while (matriz[i][j] != maximo) {
+				if (j == 0) {
+					j = m;
+					c = 0;
+					break;
 				} else {
-					if (i != 0) {
+					seq[c] = 3;
+					c++;
+					j--;
+				}
+			}
+		}
+		while (i != 0 || j != 0) {
+			if (i != 0 && j != 0) {
+				if (matriz[i][j] == matriz[i - 1][j - 1] + match || matriz[i][j] == matriz[i - 1][j - 1] + mismatch) {
+					seq[c] = 1;
+					c++;
+					i--;
+					j--;
+				} else {
+					if (matriz[i][j] == matriz[i - 1][j] + gap) {
 						seq[c] = 2;
 						c++;
 						i--;
@@ -109,51 +124,62 @@ public class AlgoritmoSemiGlobal {
 						j--;
 					}
 				}
+			} else {
+				if (i != 0) {
+					seq[c] = 2;
+					c++;
+					i--;
+				} else {
+					seq[c] = 3;
+					c++;
+					j--;
+				}
 			}
-			
-			int[] arm = new int[c];
-			int k = c-1;
-			for (i = 0; i < c; i++) {
-				arm[k] = seq[i];
-				k--;
-			}
-			seq = arm;
 		}
 
-		// metodo que imprime as sequencias alinhadas atraves do alinhamento otimo
-		public void imprimeSequencias(char[] A, char[] B) {
-			int i;
-			int k = 0;
-			
-			System.out.println();
-			for (i = 0; i < seq.length; i++) {
-				if (seq[i] == 1) {
+		int[] arm = new int[c];
+		int k = c - 1;
+		for (i = 0; i < c; i++) {
+			arm[k] = seq[i];
+			k--;
+		}
+		seq = arm;
+	}
+
+	// metodo que imprime as sequencias alinhadas atraves do alinhamento otimo
+	public void imprimeSequencias(char[] A, char[] B) {
+		int i;
+		int k = 0;
+
+		System.out.println();
+		for (i = 0; i < seq.length; i++) {
+			if (seq[i] == 1) {
+				System.out.print(A[k]);
+				k++;
+			} else {
+				if (seq[i] == 2) {
 					System.out.print(A[k]);
 					k++;
 				} else {
-					if (seq[i] == 2) {
-						System.out.print(A[k]);
-						k++;
-					} else {
-						System.out.print("-");
-					}
-				}
-			}
-			k = 0;
-			System.out.println();
-			for (i = 0; i < seq.length; i++) {
-				if (seq[i] == 1) {
-					System.out.print(B[k]);
-					k++;
-				} else {
-					if (seq[i] == 2) {
-						System.out.print("-");
-					} else {
-						System.out.print(B[k]);
-						k++;
-					}
+					System.out.print("-");
 				}
 			}
 		}
-
+		k = 0;
+		System.out.println();
+		for (i = 0; i < seq.length; i++) {
+			if (seq[i] == 1) {
+				System.out.print(B[k]);
+				k++;
+			} else {
+				if (seq[i] == 2) {
+					System.out.print("-");
+				} else {
+					System.out.print(B[k]);
+					k++;
+				}
+			}
+		}
 	}
+
+}
